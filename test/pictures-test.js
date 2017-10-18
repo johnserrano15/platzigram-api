33126de144c5fbc2109ff3381876fc3dd1791a09
john andrey <andrey_201015@hotmail.com>
2017-10-17 23:21:44 -0500

    Implementando test con ava, probando el package micro usando la version de node 6.10.3

---
+++ test/pictures-test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import test from 'ava'
+// Con micro se puede traspilar el código por eso usamos ES2015 y ES2016
+import micro, { send } from 'micro'
+import uuid from 'uuid-base62'
+// Listen se complementa con micro
+import listen from 'test-listen'
+// Request es para hacer peticiones http es muy popular
+import request from 'request-promise'
+
+// test básico para aprender a usar micro.
+test('GET /:id', async t => {
+  let id = uuid.v4()
+
+  let srv = micro(async (req, res) => {
+    // Es lo mismo que { id: id }
+    send(res, 200, { id })
+  })
+
+  let url = await listen(srv)
+  let body = await request({ uri: url, json: true })
+  t.deepEqual(body, { id })
+})
+
+test.todo('POST /')
+test.todo('POST /:id/like')
